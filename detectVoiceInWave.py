3be0b0743ca54c14f927fec6c65301ac2beca6ea
Danila Papko <36470704+dapapko@users.noreply.github.com>
2021-02-22 23:04:27 +0300

    Update detectVoiceInWave.py

--- detectVoiceInWave.py
+++ detectVoiceInWave.py
@@ -1,6 +1,7 @@
 from vad import VoiceActivityDetector
 import argparse
 import json
+from pydub import AudioSegment
 
 def save_to_file(data, filename):
     with open(filename, 'w') as fp:
@@ -18,5 +19,17 @@ if __name__ == "__main__":
     raw_detection = v.detect_speech()
     speech_labels = v.convert_windows_to_readible_labels(raw_detection)
     print(speech_labels)
-    
+    fsound = AudioSegment.from_file(args.inputfile, format="wav")
+    clean_speech = AudioSegment.empty()
+    k = 0
+    export_path = "C:\\Users\\Данила\\vad_test\\"
+    for label in speech_labels:
+        begin = label['speech_begin'] * 1000
+        end = label['speech_end'] * 1000
+        speech = fsound[begin:end]
+        clean_speech += speech
+        fname = f"{export_path}fragment_{k}.wav"
+        k+= 1
+        speech.export(fname, format="wav")
     save_to_file(speech_labels, args.outputfile)
+    clean_speech.export(f"{export_path}clean_speech.wav", format="wav")
